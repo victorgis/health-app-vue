e63766b419e6f0a452830a81b3cf0f6183becb2c
victorgis <essangvictor@gmail.com>
2024-04-09 18:07:33 +0100

    2024 updates

--- server/routes/facilityRoutes.js
+++ server/routes/facilityRoutes.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const {
   getFacilities,
   setFacility,
   updateFacility,
   deleteFacility,
-} = require('../controllers/facilitiesControllers')
+} = require("../controllers/facilitiesControllers");
 
 // const { protect } = require('../middleware/authMiddleware')
 
-router.route('/').get(getFacilities).post(setFacility)
-router.route('/:id').delete(deleteFacility).put(updateFacility)
+router.route("/").get(getFacilities).post(setFacility);
+router.route("/:id").delete(deleteFacility).put(updateFacility);
 
-module.exports = router
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,20 +1,22 @@
 // const path = require('path');
-const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv').config();
-const { errorHandler } = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db');
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT;
 
-connectDB();
+// connectDB();
 
 const app = express();
 
 //important for seeing req.body
+app.use(express.static("../health-app-vue/dist"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(errorHandler);
 
-app.use('/api/facilities', require('./routes/facilityRoutes'));
+// app.use("/api/facilities", require("./routes/facilityRoutes"));
 // app.use('/api/users', require('./routes/userRoutes'));
 
 // Serve frontend
@@ -30,6 +32,41 @@ app.use('/api/facilities', require('./routes/facilityRoutes'));
 //   app.get('/', (req, res) => res.send('Please set to production'));
 // }
 
-app.use(errorHandler);
+app.post("/api/data", (req, res) => {
+  const { name } = req.body;
+  //   res.json({ success: true, data: facility });
+  console.log(req.body);
+  if (name) {
+    return res.status(200).json({ success: true, data: req.body });
+  }
+
+  res.status(401).send("Provide name");
+  //   console.log(facility);
+});
+
+app.put("/api/data/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  // res.json({})
+  console.log(id, name);
+  const hospital = hospitals.find((hospital) => hospital.id === id);
+  const newHospital = hospital.map((hospital) => {
+    if (hospital) {
+      hospital.name = name;
+      return hospital;
+    }
+  });
+  res.status(200).json({ success: true, data: newHospital });
+});
+
+app.delete("/api/data/:id", (req, res) => {
+  const { id } = req.params;
+  const hospital = hospitals.find((hospital) => hospital.id === id);
+  if (!hospital) {
+    return res.status(404).json({ success: false, msg: "An error occured" });
+  }
+  const newHospital = hospitals.filter((hospital) => hospital.id !== id);
+  return res.status(200).json({ success: true, data: newHospital });
+});
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
